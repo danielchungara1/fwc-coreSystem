c02c64f29cc9969d4e7743e26b3fe7a46ba3e970
DanielChungara <danielchungara1@gmail.com>
2021-08-27 14:57:08 -0300

    Adding query params.

--- src/app/catalog/components/product/result-search/result-search.component.ts
+++ src/app/catalog/components/product/result-search/result-search.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProductModel} from '../product.model';
 import {ProductService} from '../product.service';
 import {ResultSearch} from '@core/interfaces/result-search';
+import {ActivatedRoute, Route, Router} from '@angular/router';
 
 @Component({
     selector: 'app-list',
@@ -10,15 +11,28 @@ import {ResultSearch} from '@core/interfaces/result-search';
 })
 export class ResultSearchComponent implements OnInit {
 
-    constructor(public productService: ProductService) {
-    }
+    products: ProductModel[];
+    searchText: string;
 
-    model: ProductModel[];
+    constructor(public productService: ProductService,
+                private activatedRoute: ActivatedRoute
+    ) {
+
+        // this.searchText = this.activatedRoute.snapshot.paramMap.get('search-text');
+        this.searchText = this.activatedRoute.snapshot.queryParamMap.get('text');
+        this.products = history.state.data;
+
+        if (!this.products) {
+            this.productService.searchAndEmit(this.searchText, 0);
+        }
+
+    }
 
     ngOnInit(): void {
         this.productService.$resultSearch.subscribe(
             (resultSearch: ResultSearch<ProductModel>) => {
-                this.model = resultSearch.page.content;
+                this.products = resultSearch.page.content;
+                this.searchText = resultSearch.searchText;
             }
         );
     }
